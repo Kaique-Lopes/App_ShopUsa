d4b76db969d654d9a12090aba6fc1be3f8a91daa
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-08-09 12:03:26 -0300

    Classes

---
+++ App_ShopUsa/App_ShopUsa/TaxesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaxesViewController.swift
+//  App_ShopUsa
+//
+//  Created by Kaique Lopes on 09/08/21.
+//
+
+import UIKit
+
+class TaxesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
