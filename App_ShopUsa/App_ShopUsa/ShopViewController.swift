e5a839699bdd41215b8394e55eacfb44dbd7f393
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-08-09 12:07:21 -0300

    Image View

--- App_ShopUsa/App_ShopUsa/ShopViewController.swift
+++ App_ShopUsa/App_ShopUsa/ShopViewController.swift
@@ -11,7 +11,6 @@ class ShopViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
