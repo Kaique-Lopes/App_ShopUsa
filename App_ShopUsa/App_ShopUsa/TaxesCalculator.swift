1192f6fb279ad8198617a803cc804ddc0080d6c6
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-08-18 20:06:12 -0300

    TaxesCalculator

--- App_ShopUsa/App_ShopUsa/TaxesCalculator.swift
+++ App_ShopUsa/App_ShopUsa/TaxesCalculator.swift
@@ -9,4 +9,13 @@ import Foundation
 
 class TaxesCalculator {
     
+    static let shared = TaxesCalculator()
+    var dolar: Double = 3.5
+    var iof: Double = 6.38
+    var stateTax: Double = 7.0
+    var shoppingValue: Double = 0
+    
+    private init() {
+        
+    }
 }
